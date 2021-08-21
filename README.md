6cd52a9bfbcb7ab62bbd056188282fced7bd7530
Itacir Pompeu <pompeulimp@gmail.com>
2021-08-21 17:56:22 -0300

    aula-01 final

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# aprenda_express
+# Aprenda_express
+
 mongoose, expressjs, nodejs, jwt, jest, tdd
+
+# Aula 01 Criando o basico.
+
+# Aula 02 Acesso ao mongo, separando responsabilidades.
--- app.js
+++ app.js
@@ -2,21 +2,30 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-const User = require("./src/models/user");
-const uri = "mongodb://localhost:27017/aprenda_express";
+const fs = require("fs");
+const password = fs.readFileSync("./.pass", "utf-8");
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
+const User = require("./src/models/user");
+const uri = `mongodb+srv://javaria:${password.trim()}@cluster0.b9snm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .catch(console.error);
 
 const createUserController = (req, res) => {
   return User.create(req.body)
     .then((user) => res.status(201).json(user))
     .catch((err) => res.status(500).json({ erros: [err.message] }));
 };
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.post("/api/user", createUserController);
 
