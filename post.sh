5e0ec58d10c3ebc05617edc71ce45b1286654861
Itacir Pompeu <pompeulimp@gmail.com>
2021-09-07 09:57:36 -0300

    user create with jest

--- post.sh
+++
@@ -1,4 +0,0 @@
-curl --header "Content-Type: application/json" \
-  --request POST \
-  --data '{"name":"Joana","email":"joanalinda@net.com","password": "123456"}' \
-  http://localhost:3000/api/user
