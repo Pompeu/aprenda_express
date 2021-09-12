f1477448177d5bb4c93bedc376ef21d3900a0b91
Itacir Pompeu <pompeulimp@gmail.com>
2021-09-12 16:21:04 -0300

    add prettier

--- post_jwt.sh
+++
@@ -1,4 +0,0 @@
-curl --header "Content-Type: application/json" \
-  --request POST \
-  --data '{"email":"ia@net.com", "password": "jepoajepa"}' \
-  http://localhost:3000/api/jwt
--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
 })
 
-module.exports = mongoose.model('User', UserSchema)
+module.exports = mongoose.model("User", UserSchema)
